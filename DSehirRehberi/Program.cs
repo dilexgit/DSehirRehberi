01a531477da55ef826a362fde415c75e51bd7f82
Muhammed <univercityclub@hotmail.com>
2023-10-23 00:54:03 +0300

    yorum

--- DSehirRehberi/Program.cs
+++ DSehirRehberi/Program.cs
@@ -14,6 +14,7 @@ namespace DSehirRehberi
         public static void Main(string[] args)
         {
             CreateHostBuilder(args).Build().Run();
+            //yorum
         }
 
         public static IHostBuilder CreateHostBuilder(string[] args) =>
